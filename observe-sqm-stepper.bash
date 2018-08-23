e4ba922099551081a8f704a557c7be78f6ad7574
aubema <aubema@localhost>
2018-08-23 19:48:39 +0000

    debuggage complet de observe

--- observe-sqm-stepper.bash
+++ observe-sqm-stepper.bash
@@ -17,8 +17,89 @@
 #
 #    Contact: martin.aube@cegepsherbrooke.qc.ca
 #
-# 
+# =============================
+# reset the center of the clear filter
+recenter () {
+      echo "recenter" $possqm
+      let ang=possqm-maxstep/8-pos
+      if [ $ang -ge $maxstep ] 
+      then let ang=ang-maxstep
+      fi
+      if [ $ang -le -$maxstep ]
+      then let ang=ang+maxstep
+      fi
+      # moving filter wheel
+      echo "Moving the filter wheel to filter before SQM to recenter" 
+      let pos=pos+ang
+      if [ $pos -ge $maxstep ] 
+      then let pos=pos-maxstep
+      fi
+      if [ $pos -le -$maxstep ]
+      then let pos=pos+maxstep
+      fi
+      /usr/local/bin/MoveStepFilterWheel.py $ang 0  
+      let newsteps=maxsteps/8/2/movestep
+      findSQM $newsteps
+}
+# ============================
+# find the clear filter position
+findSQM () {
+     let nstep=$1
+     memoi=3000
+     n=0
+     while [ $n -lt $nstep ] 
+     do /usr/local/bin/sqmleread.pl $sqmip 10001 1 > sqmdata.tmp    
+          read sqm < sqmdata.tmp
+          echo $sqm | sed 's/,/ /g' | sed 's/m/ /g' | sed 's/\./ /g' > toto.tmp
+          read bidon sqmm sqmd bidon < toto.tmp
+         # remove leading zero to the sky brightness
+          if [ ${sqmm:0:1} == 0 ]
+          then sqmm=`echo $sqmm | sed 's/0//g'`
+          fi
+          if [ ${sqmd:0:1} == 0 ]
+          then sqmd=`echo $sqmd | sed 's/0//g'`
+          fi
+          let meas=sqmm*100+sqmd
+          if [ $meas -lt $memoi ]
+          then let memoi=meas
+                   let possqm=pos
+                   echo "Found clearer position = " $possqm
+          fi
+          /usr/local/bin/MoveStepFilterWheel.py $movestep 0
+          let pos=pos+movestep
+          if [ $pos -ge $maxstep ] 
+          then let pos=pos-maxstep
+          fi
+          if [ $pos -le -$maxstep ] 
+          then let pos=pos+maxstep
+          fi
+          let n=n+1
+         done
+         echo "Clearest filter position +- "$movestep " = " $possqm
+}
+# ==================================
+# global positioning system
+globalpos () {
+#
+#            reading 10 gps transactions
+#
+             /bin/echo "Waiting 5 sec for GPS reading..."
+             /usr/bin/gpspipe -w -n 10 > $homed/public_html/cgi-bin/coords.tmp
+             /usr/bin/tail -1 $homed/public_html/cgi-bin/coords.tmp | sed 's/,/\n/g' | sed 's/"//g' | sed 's/:/ /g'> $homed/public_html/cgi-bin/bidon.tmp
 
+             /bin/rm -f $homed/public_html/cgi-bin/coords.tmp
+             grep lat $homed/public_html/cgi-bin/bidon.tmp > $homed/public_html/cgi-bin/bidon1.tmp
+             read bidon lat bidon1 < $homed/public_html/cgi-bin/bidon1.tmp
+             grep lon $homed/public_html/cgi-bin/bidon.tmp > $homed/public_html/cgi-bin/bidon1.tmp
+             read bidon lon bidon1 < $homed/public_html/cgi-bin/bidon1.tmp
+             grep alt $homed/public_html/cgi-bin/bidon.tmp > $homed/public_html/cgi-bin/bidon1.tmp
+             read bidon alt bidon1 < $homed/public_html/cgi-bin/bidon1.tmp
+             /bin/echo "GPS is connected, reading lat lon data."
+             /bin/echo "GPS gives Latitude:" $lat ", Longitude:" $lon "and Altitude:" $alt
+}
+# ==================================
+# ==================================
+# main
 # home directory
 homed=/home/sand
 # activate gps option 0=off 1=on
@@ -26,13 +107,15 @@ gpsf=1
 nobs=9999  		# number of times measured if 9999 then infinity
 waittime=10             # at a mag of about 24 the integration time is around 60s
 movestep=16
-maxstep=2048
+maxstep=2040
+daydelay=20    # add a delay between samplings during daytime to restrict the total amount of data
 #
 # set band list
 # wavelengths 0:= Clear ,1:= Red 2:= Green ,3:= Blue ,4:= Yellow
 #
 filters=( 0 1 2 3 4 )
 calib=( 1.0 1.0 1.0 1.0 1.0 )
+fname=(Clear Red Green Blue Yellow)
 grep filter_gain $homed/localconfig > toto
 read bidon gain bidon < toto
 grep filter_offset $homed/localconfig > toto
@@ -40,155 +123,106 @@ read bidon offset bidon < toto
 grep sqmIP $homed/localconfig > toto # sqmIP est le mot cle cherche dans le localconfig 
 read bidon sqmip bidon < toto
 # find the clear filter
-# one complete rotation in half step mode (mode 1) is maxstep=4096 i.e. 1 step = 0.087890625 deg
-# if you use the full step mode (mode 0) then maxstep=2048 is the number of steps i.e. 1 step = 0.17578125
-pos=0
-memoi=3000
-n=0
+# one complete rotation in half step mode (mode 1) is maxstep=4080 i.e. 1 step = 0.087890625 deg
+# if you use the full step mode (mode 0) then maxstep=2040 is the number of steps i.e. 1 step = 0.17578125
 let nstep=maxstep/movestep
-while [ $n -lt $nstep ] 
-do /usr/local/bin/sqmleread.pl $sqmip 10001 1 > sqmdata.tmp    
-   read sqm < sqmdata.tmp
-   echo $sqm | sed 's/,/ /g' | sed 's/m/ /g' | sed 's/\./ /g' > toto.tmp
-   read bidon sqmm sqmd bidon < toto.tmp
-   # remove leading zero to the sky brightness
-   if [ ${sqmm:0:1} == 0 ]
-   then sqmm=`echo $sqmm | sed 's/0//g'`
-   fi
-   if [ ${sqmd:0:1} == 0 ]
-   then sqmd=`echo $sqmd | sed 's/0//g'`
-   fi
-   let meas=sqmm*100+sqmd
-   if [ $meas -lt $memoi ]
-   then let memoi=meas
-        let possqm=pos
-        echo "Found clearer position = " $possqm
-   fi
-   sudo MoveStepFilterWheel.py $movestep 0
-   let pos=pos+movestep
-   if [ $pos -ge $maxstep ] 
-   then let pos=pos-maxstep
-   fi
-   if [ $pos -le -$maxstep ] 
-   then let pos=pos+maxstep
-   fi
-   let n=n+1
-done
-echo "Clear filter position +- "$movestep " = " $possqm
+pos=0
+findSQM $nstep
 #
 #  searching for gps port
 #
-   if [ $gpsf -eq 1 ] 
-   then echo "GPS mode activated"
-        if [ `ls /dev | grep ttyUSB0`  ] 
-        then echo "GPS look present." 
-#
-#            reading 10 gps transactions
-#
-             /bin/echo "Waiting 5 sec for GPS reading..."
-             /usr/bin/gpspipe -w -n 10 > $homed/public_html/cgi-bin/coords.tmp
-             /usr/bin/tail -1 $homed/public_html/cgi-bin/coords.tmp > $homed/public_html/cgi-bin/bidon.tmp
-             /bin/rm -f $homed/public_html/cgi-bin/coords.tmp
-             read bidon bidon bidon lat lon altitude bidon1 < $homed/public_html/cgi-bin/bidon.tmp
-             if [ "${bidon1:0:1}" != "" ]
-             then /bin/echo "GPS is connected, reading lat lon data."
-                  lon=`/bin/echo $lon"/-1" |/bin/bc -l`
-                  DD=`/bin/echo "scale=0;"$lon"/1" |/usr/bin/bc -l`
-                  dd=`/bin/echo "scale=0;"$lat"/1" |/usr/bin/bc -l`
-                  MMM=`/bin/echo "("$lon"-"$DD")*60" |/usr/bin/bc -l`
-                  MM=${MMM:0:2}
-                  mmm=`/bin/echo "("$lat"-"$dd")*60" |/usr/bin/bc -l`
-                  mm=${mmm:0:2}
-                  SSS=`/bin/echo "(("$lon"-"$DD")-"$MM"/60)*3600" |/usr/bin/bc -l`
-                  SS=${SSS:0:2}
-                  sss=`/bin/echo "(("$lat"-"$dd")-"$mm"/60)*3600" |/usr/bin/bc -l`
-                  ss=${sss:0:2}
-                  /bin/echo "GPS give Latitude:" $dd $mm $ss ", Longitude:" $DD $MM $SS "and Altitude:" $altitude
-             else /bin/echo "GPS not working: using coords. from localconfig"
-                  /bin/echo "Latitude:" $dd $mm $ss ", Longitude:" $DD $MM $SS
-             fi    
-        else /bin/echo "GPS not present: using coords. from localconfig"
-             /bin/echo "Latitude:" $dd $mm $ss ", Longitude:" $DD $MM $SS
-        fi
-   else  echo "GPS mode off"
-   fi
-   /bin/grep "Site_name" $homed/localconfig > $homed/public_html/cgi-bin/ligne.tmp
-   read bidon NAME bidon < $homed/public_html/cgi-bin/ligne.tmp
-#
-#  reading longitude and latitude from observation schedule
-#
-   if [ `grep -c " " $homed/public_html/cgi-bin/$myFile` -ne 0 ]
-   then /bin/grep Longitude $homed/localconfig > $homed/public_html/cgi-bin/ligne.tmp
-        read bidon DD MM SS bidon < $homed/public_html/cgi-bin/ligne.tmp
-        /bin/grep Latitude $homed/localconfig > $homed/public_html/cgi-bin/ligne.tmp
-        read bidon dd mm ss bidon < $homed/public_html/cgi-bin/ligne.tmp
-   else 
-        echo "Please put something in $homed/localconfig and restart observe-sqm-stepper.bash."
-   fi
-
-# according to unihedron here are the typical waiting time vs sky brightness
-# 19.83 = 1s
-# 21.97 = 6.9s
-# 22.69 = 12.8s
-# 23.13 = 18.7s
-# 23.48 = 24.6s
-# 23.76 = 30.5s
-# 24.00 = 36.4s
-# 24.21 = 42.3s
-# 24.41 = 48.2s 
-# 24.60 = 54.1s
-# 24.76 = 60s
-#
-# it is suggested to use filter 1 (Red) to estimate the waittime
-# waittime must be at least twice that time
-# moving the filter wheel to the Red filter
-# 72 degrees between filter i.e. maxstep/5 
-let increment=maxstep/5
-echo "increment=" $increment
-let ang=possqm+1*increment-pos
-echo "Moving wheel of" $ang " steps"
-#
-/usr/local/bin/MoveStepFilterWheel.py $ang 0
-let pos=pos+ang
-if [ $pos -ge $maxstep ] 
-then let pos=pos-maxstep
+if [ $gpsf -eq 1 ] 
+then echo "GPS mode activated"
+         if [ `ls /dev | grep ttyUSB0`  ] 
+         then echo "GPS look present." 
+                  globalpos
+         else /bin/echo "GPS not present: using coords. from localconfig"
+                #
+                #  reading longitude and latitude from localconfig
+                #
+                if [ `grep -c " " $homed/public_html/cgi-bin/$myFile` -ne 0 ]
+                then /bin/grep Longitude $homed/localconfig > $homed/public_html/cgi-bin/ligne.tmp
+                         read bidon lon bidon < $homed/public_html/cgi-bin/ligne.tmp
+                         /bin/grep Latitude $homed/localconfig > $homed/public_html/cgi-bin/ligne.tmp
+                         read bidon lat bidon < $homed/public_html/cgi-bin/ligne.tmp
+                         /bin/grep Altitude $homed/localconfig > $homed/public_html/cgi-bin/ligne.tmp
+                        read bidon alt bidon < $homed/public_html/cgi-bin/ligne.tmp
+                else 
+                        echo "Please put something in "$homed"/localconfig and restart observe-sqm-stepper.bash."
+                fi
+                /bin/echo "Latitude:" $lat ", Longitude:" $lon
+         fi
+else  echo "GPS mode off"
 fi
-if [ $pos -le -$maxstep ]
-then let pos=pos+maxstep
-fi
-echo "Waiting " $waittime " s to estimate final acquisition time"
-/bin/sleep $waittime
-/usr/local/bin/sqmleread.pl $sqmip 10001 1 > sqmdata.tmp
-read sqm < sqmdata.tmp
-echo $sqm | sed 's/,/ /g' | sed 's/s//g' > toto.tmp
-read bidon bidon bidon bidon tim bidon < toto.tmp
-echo "Decimal readout time: " $tim  
-# default wait time set to the acquisition time with the red filter
-echo $tim | sed 's/\./ /g'  > toto.tmp
-read tim timd toto < toto.tmp
-echo $tim | sed 's/000//g'  > toto.tmp
-read tim toto < toto.tmp
-if [ $timd -ge 500 ]
-then let tim=tim+1
-fi
-# add 2 seconds to the waiting time to be sure that no overlap will occur
-let waittime=tim+2
-echo "Required acquistion time:" $waittime
+/bin/grep "Site_name" $homed/localconfig > $homed/public_html/cgi-bin/ligne.tmp
+read bidon NAME bidon < $homed/public_html/cgi-bin/ligne.tmp
 #
-# Main loop
+# loop
 #
 i=0
+count=0
 while [ $i -lt $nobs ]
-do if [  $nobs == 9999 ] 
-      then let nobs=i+1  # on s assure que nobs est toujours = ou > a i
+do  let count=count+1
+      if [ $count -eq 10 ]   # set frequency of the recenter operation
+      then recenter
+               let count=0
+      fi
+      #  according to unihedron here are the typical waiting time vs sky brightness
+      #  19.83 = 1s
+      #  21.97 = 6.9s
+      #  22.69 = 12.8s
+      #  23.13 = 18.7s
+      #  23.48 = 24.6s
+      #  23.76 = 30.5s
+      #  24.00 = 36.4s
+      #  24.21 = 42.3s
+      #  24.41 = 48.2s 
+      #  24.60 = 54.1s
+      #  24.76 = 60s
+      #
+      #  it is suggested to use filter 1 (Red) to estimate the waittime
+      #  waittime must be at least twice that time
+      #  moving the filter wheel to the Red filter
+      #  72 degrees between filter i.e. maxstep/5 
+      let ang=possqm+1*maxstep/5-pos
+      echo "Moving wheel of" $ang " steps"
+      #
+      /usr/local/bin/MoveStepFilterWheel.py $ang 0
+      let pos=pos+ang
+      if [ $pos -ge $maxstep ] 
+      then let pos=pos-maxstep
+      fi
+      if [ $pos -le -$maxstep ]
+      then let pos=pos+maxstep
+      fi
+      let waittime=10
+      echo "Waiting " $waittime " s to determine optimal acquisition time"
+      /bin/sleep $waittime
+      /usr/local/bin/sqmleread.pl $sqmip 10001 1 > sqmdata.tmp
+      read sqm < sqmdata.tmp
+      echo $sqm | sed 's/,/ /g' | sed 's/s//g' > toto.tmp
+      read bidon bidon bidon bidon tim bidon < toto.tmp
+      echo "Decimal readout time: " $tim  
+      # default wait time set to the acquisition time with the red filter
+      echo $tim | sed 's/\./ /g'  > toto.tmp
+      read tim timd toto < toto.tmp
+      echo $tim | sed 's/000//g'  > toto.tmp
+      read tim toto < toto.tmp
+      if [ $timd -ge 500 ]
+      then let tim=tim+1
+      fi
+      #   add 1 seconds to the waiting time to be sure that no overlap will occur
+      let waittime=tim+1
+      echo "Required acquistion time:" $waittime
+      if [  $nobs != 9999 ] 
+      then let i=i+1 #   never ending loop
       fi
       n=0
       echo "Start"
-      let i=i+1
       echo "Observation number: " $i
       while [ $n -lt ${#filters[*]} ]
       do filter=${filters[$n]}
-           let ang=possqm+n*increment-pos
+           let ang=possqm+n*maxstep/5-pos
            if [ $ang -ge $maxstep ] 
            then let ang=ang-maxstep
            fi
@@ -196,7 +230,7 @@ do if [  $nobs == 9999 ]
       then let ang=ang+maxstep
       fi
       # moving filter wheel
-      echo "Moving the filter wheel to filter " $n
+      echo "Moving the filter wheel to filter " $n "("${fname[$n]}")"
       let pos=pos+ang
       if [ $pos -ge $maxstep ] 
       then let pos=pos-maxstep
@@ -227,15 +261,28 @@ do if [  $nobs == 9999 ]
    done
    echo $sb | sed 's/\./ /g'  > toto.tmp  # on decoupe les entiers et decimales de la mesure sqm
    read seuil toto toto < toto.tmp
-   if [ $seuil -lt 12 ]
-       then /bin/sleep 100    # waiting at least 100 sec when it is daytime
-   fi 
    nomfich=`date -u +"%Y-%m-%d"`
    nomfich=$nomfich".txt"
    time=`date +%Y-%m-%d" "%H:%M:%S`
-   echo $time $sqmreading ${sbcals[0]} ${sbcals[1]} ${sbcals[2]} ${sbcals[3]} ${sbcals[4]}>> $homed/$nomfich
+   y=`date +%Y`
+   mo=`date +%m`
+   d=`date +%d`
+   if [ ! -d /home/sand/public_html/data/$y ]
+   then mkdir /home/sand/public_html/data/$y
+   fi
+   if [ ! -d /home/sand/public_html/data/$y/$mo ]
+   then /bin/mkdir /home/sand/public_html/data/$y/$mo
+   fi
+   echo $time $lat $lon $alt $sqmreading ${sbcals[0]} ${sbcals[1]} ${sbcals[2]} ${sbcals[3]} ${sbcals[4]}>> $homed/public_html/data/$y/$mo/$nomfich
+   if [ $seuil -lt 12 ]
+       then /bin/sleep $daydelay    # waiting when it is daytime
+   fi 
 done
 echo "Parking filter wheel..."
 let ang=-pos
 /usr/local/bin/MoveStepFilterWheel.py $ang 0  
 echo "Finish observe-sqm-stepper.bash"
+exit 0
+
+
+
