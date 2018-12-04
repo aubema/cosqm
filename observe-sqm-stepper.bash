992fdc45d35dd915b8223cd6012e13f1433e23bb
aubema <aubema@localhost>
2018-12-04 00:29:35 -0500

    try to accelerate sqm finding

--- observe-sqm-stepper.bash
+++ observe-sqm-stepper.bash
@@ -243,7 +243,7 @@ do    #  according to unihedron here are the typical waiting time vs sky brightn
       let waittime=tim+1
       echo "Required acquistion time:" $waittime
       let count=count+1
-      if [ $count -eq 10 ]   # set frequency of the recenter operation
+      if [ $count -eq 2 ]   # set frequency of the recenter operation
       then findSQM
            let count=0
            #
