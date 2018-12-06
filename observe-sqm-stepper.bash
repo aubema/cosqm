7535ce8c6739eef6794aa2292f728c2756868889
aubema <aubema@localhost>
2018-12-06 22:59:47 +0000

    debug important changes

--- observe-sqm-stepper.bash
+++ observe-sqm-stepper.bash
@@ -72,55 +72,148 @@ findIntegration () {
 # find the clear filter position
 #
 findSQM () {
-     avgnum=10 # number of scan to average
+     declare -a data
+     declare -a posi
+     avgnum=9 # number of scan to average
      nscan=1
+     averagesqm=0
+     deltasqm=0
+     ndelta=0
      declare -a minim
      nmoy=1
      let pauseflag=0 
      filteroffset=0  # to ensure that the SQM fall in the center of the filter
      let movestep=maxstep/128
-     let nstep=10*maxstep/movestep
+     let nstep=maxstep/movestep-1
      # determine the relevant waittime
      findIntegration
      echo "Searching SQM position..."
      while [ $nmoy -le $avgnum ] 
      do echo "Searching trial #" $nmoy
 	echo "Searching number of steps: " $nstep "Skipping " $movestep "tics" 
-        memoi=3000
+
+
+
+        # scan the filter wheel and store the data into arrays
         n=0
         while [ $n -le $nstep ] 
         do findIntBrightness 
-	   let seuil100=seuil*100
-	   let maxim100=maxim*100
-	   if [ $meas -lt $seuil100 ] && [ $meas -gt $maxim100 ] 
-           then pauseflag=1
-	   fi
 	   echo $n $pos $meas
-           if [ $meas -lt $memoi ]
-           then let memoi=meas
-                let possqm=pos
-                # echo "Found clearer position = " $possqm
-           fi
+	   posi[$n]=$pos
+	   data[$n]=$meas
            /usr/local/bin/MoveStepFilterWheel.py $movestep 0
            let pos=pos+movestep
-           if [ $pos -ge $maxstep ] 
-           then let pos=pos-maxstep
+           if [ $pos -gt $maxstep ] 
+           then let pos=pos-maxstep+1
            fi
-           if [ $pos -le -$maxstep ] 
-           then let pos=pos+maxstep
+           if [ $pos -lt -$maxstep ] 
+           then let pos=pos+maxstep+1
            fi
            let n=n+1
         done
+
+
+
+
+        # scan arrays to correct the decreasing trend along the scan
+        n=0
+        let variation=data[$nstep]-data[0]
+        while [ $n -le $nstep ] 
+        do let data[$n]=data[$n]-variation*n/nstep
+	   let n=n+1
+        done
+
+
+
+
+        # scan arrays to find the minimum 
+	memoi=3000
+        n=0
+        while [ $n -le $nstep ] 
+        do if [ ${data[$n]} -lt $memoi ]
+           then let memoi=data[$n]
+                let possqm=posi[$n]
+                # echo "Found clearer position = " $possqm
+           fi
+           let n=n+1
+        done
+
+
+
+
+echo "Found clearer position = " $possqm
+	let n=0
+	let ang=-maxstep/5
+	let newstep=maxstep/5/movestep   # move before the preceeding peak
+	/usr/local/bin/MoveStepFilterWheel.py $ang 0
+	let pos=pos+ang
+        if [ $pos -gt $maxstep ] 
+        then let pos=pos-maxstep+1
+        fi
+        if [ $pos -lt -$maxstep ] 
+        then let pos=pos+maxstep+1
+        fi
+
+        let memoi=0
+        while [ $n -le $newstep ]
+	do findIntBrightness
+	   if [ $meas -gt $memoi ]
+           then let memoi=meas
+                let pospeak=pos
+           fi
+           /usr/local/bin/MoveStepFilterWheel.py $movestep 0
+           let pos=pos+movestep
+           if [ $pos -gt $maxstep ] 
+           then let pos=pos-maxstep+1
+           fi
+           if [ $pos -lt -$maxstep ] 
+           then let pos=pos+maxstep+1
+           fi
+
+           let n=n+1
+	done
+           if [ $pospeak -gt $maxstep ] 
+           then let pospeak=pospeak-maxstep+1
+           fi
+           if [ $pospeak -lt -$maxstep ] 
+           then let pospeak=pospeak+maxstep+1
+           fi
+
+        # add 1/10 of the total tics to find the center of the filter
+	let possqm=pospeak+maxstep/10
+        if [ $possqm -gt $maxstep ] 
+        then let possqm=possqm-maxstep+1
+        fi
+        if [ $possqm -lt -$maxstep ] 
+        then let possqm=possqm+maxstep+1
+        fi
+
         echo "Clearest filter position +- "$movestep " = " $possqm
-   	let possqm=possqm-filteroffset
 	minim[$nmoy]=$possqm
 	let nmoy=nmoy+1
         echo "SQM position:" $possqm "scan no. " $nscan
 	let nscan=nscan+1
-	let finalsqm=finalsqm+possqm
+	let averagesqm=averagesqm+possqm
      done
-     let possqm=finalsqm/avgno
-     echo "Average SQM position:" $possqm
+     let averagesqm=averagesqm/avgnum
+     for i in ${minim[*]}
+     do  let diffsqm=(i-averagesqm)
+	 diffsqm=`echo $diffsqm | tr -d -`  # take the absolute value 
+         let deltasqm=deltasqm+diffsqm
+     done
+     let deltasqm=deltasqm/avgnum
+# remove value too far from the average
+     for i in ${minim[*]}
+     do  let diffsqm=(i-averagesqm)
+	 diffsqm=`echo $diffsqm | tr -d -`  # take the absolute value 
+         if [ $diffsqm -le $deltasqm ]
+         then let ndelta=ndelta+1
+	      let finalsqm=finalsqm+i
+	 fi
+     done
+     let possqm=finalsqm/ndelta
+     echo "Average SQM position:" $possqm "(was " $averagesqm "before statistical sorting)"i
+     echo "Variability:" $deltasqm "initial scans" $avgnum "final scans" $ndelta
 }
 # ==================================
 # global positioning system
@@ -160,17 +253,17 @@ globalpos () {
 # main
 # activate gps option 0=off 1=on
 gpsf=1
+gpsport="ttyACM0"
 nobs=9999  		# number of times measured if 9999 then infinity
 waittime=10             # at a mag of about 24 the integration time is around 60s
 movestep=16
-maxstep=2040
-filteroffset=0  # to ensure that the SQM fall in the center of the filter
+maxstep=2048
 # We search for the SQM position of the filter wheel during twilight (around SB=12)
 # At that moment the sky is relatively uniform and the integration time is short
 # maxim and minim should be written as 100xSkyBrightness (e.g for Sky brightness of 20.3 you 
 # should write 2030
-minim=1200 # minimal value of the interval of sky brightness optimal to find SQM position
-maxim=1250 # maximal value of the inverval of sky brightness optimal to find SQM position 
+minim=600 # minimal value of the interval of sky brightness optimal to find SQM position
+maxim=2200 # maximal value of the inverval of sky brightness optimal to find SQM position 
 #
 # set band list
 # wavelengths 0:= Clear ,1:= Red 2:= Green ,3:= Blue ,4:= Yellow
@@ -189,7 +282,7 @@ pos=0
 #
 if [ $gpsf -eq 1 ] 
 then echo "GPS mode activated"
-     if [ `ls /dev | grep ttyACM0`  ] 
+     if [ `ls /dev | grep $gpsport`  ] 
      then echo "GPS look present." 
           globalpos
      else /bin/echo "GPS not present: using coords. from localconfig"
@@ -221,7 +314,7 @@ do    findIntegration
       findIntBrightness
       while [ $meas -le $minim ]    # too bright it is daytime
       do findIntBrightness
-	 echo "Wait 15 min until a bit of darkness"
+	      echo "Wait 15 min until twilight ("$minim"<(SBx100)<"$maxim")"
 	 sleep 900
       done
       if [ $meas -lt $maxim ]
@@ -235,7 +328,7 @@ do    findIntegration
       #
       if [ $gpsf -eq 1 ] 
       then echo "GPS mode activated"
-           if [ `ls /dev | grep ttyACM0`  ] 
+           if [ `ls /dev | grep $gpsport`  ] 
            then echo "GPS look present." 
                 globalpos
            else /bin/echo "GPS not present: using coords. from localconfig"
@@ -264,19 +357,19 @@ do    findIntegration
       while [ $n -lt ${#filters[*]} ]
       do filter=${filters[$n]}
          let ang=possqm+n*maxstep/5-pos
-         if [ $ang -ge $maxstep ] 
+         if [ $ang -gt $maxstep ] 
          then let ang=ang-maxstep
          fi
-         if [ $ang -le -$maxstep ]
+         if [ $ang -lt -$maxstep ]
          then let ang=ang+maxstep
          fi
          # moving filter wheel
          echo "Moving the filter wheel to filter " $n "("${fname[$n]}")"
          let pos=pos+ang
-         if [ $pos -ge $maxstep ] 
+         if [ $pos -gt $maxstep ] 
          then let pos=pos-maxstep
          fi
-         if [ $pos -le -$maxstep ]
+         if [ $pos -lt -$maxstep ]
          then let pos=pos+maxstep
          fi
          echo "Moving to position " $pos
@@ -304,8 +397,6 @@ do    findIntegration
          echo "Flux in band " $n " = "${sbcals[$n]}
          let n=n+1
       done
-      echo ${sqmread[0]} | sed 's/\./ /g'  > /root/toto.tmp  # on decoupe les entiers et decimales de la mesure sqm
-      read seuil toto toto < /root/toto.tmp
       nomfich=`date -u +"%Y-%m-%d"`
       nomfich=$nomfich".txt"
       time=`date +%Y-%m-%d" "%H:%M:%S`
