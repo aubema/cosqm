b531cbf94128c5d579d8fd416570898f3e1067c0
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2019-07-21 21:25:48 -0400

    change path

--- archive_cosqm.bash
+++ archive_cosqm.bash
@@ -3,7 +3,7 @@
 #program to archive cosqm network data
 cosqm_addr=(192.168.0.100 161.72.128.52 vpn.izana.org:5080 193.146.80.130:5180 )
 cosqm_name=(Saint-Camille Observatorio_Teide Santa-Cruz_Tenerife Pico_Teide)
-cd /var/www/html/DATA-SAND/CoSQM-Network
+cd /var/www/html/DATA/CoSQM-Network
 n=0
 while [ $n -le ${#cosqm_addr[*]} ]
 do addr=${cosqm_addr[$n]}
