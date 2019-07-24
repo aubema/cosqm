0cc8eadb6c7287f6aca9923497d03129d975c345
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2019-07-24 17:13:59 -0400

    improvement to archive cosqm

--- archive_cosqm.bash
+++ archive_cosqm.bash
@@ -1,6 +1,8 @@
 #!/bin/bash 
 #
 #program to archive cosqm network data
+y=`/bin/date -u +%Y`
+mo=`/bin/date -u +%m`
 cosqm_addr=(192.168.0.100 161.72.128.52 vpn.izana.org:5080 193.146.80.130:5180 )
 cosqm_name=(Saint-Camille Observatorio_Teide Santa-Cruz_Tenerife Pico_Teide)
 cd /var/www/html/DATA/CoSQM-Network
@@ -11,8 +13,8 @@ do addr=${cosqm_addr[$n]}
    let n=n+1
    echo "Downloading " $name " ..." $n"/"${#cosqm_addr[*]}
    rm -fr $addr
-   wget -q -r -N -R "index.html*" http://$addr/data/
+   wget -np -q --cut-dirs 3 -r  -R "index.html*" http://$addr/data/$y/$mo/
    echo $name " done."
-   rm -fr $name
-   mv -f $addr $name
+   cp -fr $addr/* $name/data/$y/$mo/
+   rm -fr $addr
 done
