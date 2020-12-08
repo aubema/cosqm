1c875a5ab039a3fa65a5fdf3ac4d76fe69276047
aubema <martin.aube@cegepsherbrooke.qc.ca>
2020-12-08 20:07:34 +0000

    version working well

--- Camera.bash
+++ Camera.bash
@@ -36,34 +36,34 @@ then /bin/mkdir /var/www/html/data/$y/$mo/webcam
 fi
 noname=`date +%Y-%m-%d_%H:%M:%S`
 cd /home/sand
-/usr/bin/raspistill -n -o obstmp.jpg  # -ss can be used to define the exposure time in microseconds 
-/usr/bin/convert -resize 640x640^ -gaussian-blur 0.05 -quality 85%  obstmp.jpg sky.jpg
-rm -f obstmp.jpg
+itime=239
+ng=1
+luminosite=0
+while [ $luminosite -lt 50 ] && [ $ng -le 6 ]
+do /usr/bin/raspistill -t 1 -md 3 -bm -ex off -ag 16 --shutter $itime -ISO 800 -st -o /home/sand/skytmp.jpg 
+   /usr/bin/convert -resize 640x640^ -gaussian-blur 0.05 -quality 85%  /home/sand/skytmp.jpg /home/sand/sky.jpg
 
-# mesurer le niveau de gris moyen (mean) sur cammodel
-/usr/bin/identify -verbose /home/sand/sky.jpg | /bin/grep mean | /bin/sed 's/mean://g' |  /usr/bin/tr -d '\n' > /home/sand/mean.tmp
-read r rr g gg b bb < /home/sand/mean.tmp 
-/bin/echo $r $g $b
-/bin/echo $r $g $b >> /home/sand/color.txt
-if [ ! $b ]
+
+# mesurer le niveau de gris moyen (mean)
+   /usr/bin/identify -verbose /home/sand/sky.jpg | /bin/grep mean | /bin/sed 's/mean://g' |  /usr/bin/tr -d '\n' > /home/sand/mean.tmp
+   read r rr g gg b bb < /home/sand/mean.tmp 
+cat /home/sand/mean.tmp
+   /bin/echo $r $g $b
+   /bin/echo $r $g $b >> /home/sand/color.txt
+   if [ ! $b ]
 #support des image grayscale
-then let mean=r
-else mean=`/bin/echo "scale=0;("$r"+"$g"+"$b")/3." | /usr/bin/bc -l` 
-fi
-if [ ! $mean ] ; then mean=0 ; fi
-/bin/echo $mean > /home/sand/webcam-mean
-
-echo $mean | sed 's/\./ /g' > /home/sand/toto.tmp
-read luminosite bidon < /home/sand/toto.tmp
-#
-#
-#
-if [ $luminosite -gt 50 ]
-then /bin/chmod a+rx /home/sand/sky.jpg
-     /bin/cp -f /home/sand/sky.jpg /home/sand/lastwebcam.jpg
-     /bin/chmod a+rx /home/sand/lastwebcam.jpg
-     /usr/bin/convert -resize 280x200 /home/sand/lastwebcam.jpg /home/sand/webcamsmall.jpg
-     /bin/chmod a+rx /home/sand/webcamsmall.jpg
-     mv /home/sand/sky.jpg /var/www/html/data/$y/$mo/webcam/$noname".jpg"
-fi
+   then let mean=r
+   else mean=`/bin/echo "scale=0;("$r"+"$g"+"$b")/3." | /usr/bin/bc -l` 
+   fi
+   echo $mean | sed 's/\./ /g' > /home/sand/mean.tmp
+   read luminosite bidon < /home/sand/mean.tmp
+   if [ ! $luminosite ] 
+   then luminosite=0
+   fi
+   echo $luminosite $itime
+   let itime=itime*10
+   let ng=ng+1
+done
+mv /home/sand/sky.jpg /var/www/html/data/$y/$mo/webcam/$noname".jpg"
 /bin/rm -f /home/sand/mean.tmp
+/bin/rm -f /home/sand/skytmp.jpg
