aa5aa3b5cab134776087bc178f748de9ecaa2b81
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2022-02-09 22:06:02 -0500

    ident problem

--- zero_pos.py
+++ zero_pos.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # usage: zero_filter.py
 # find the clear filter and stop there
 #
@@ -31,36 +31,36 @@ GPIO.setup(sensor_gpio, GPIO.IN)
 
 # Function for step sequence
 def setStep(w1, w2, w3, w4):
-  time.sleep(delay)
-  GPIO.output(coil_A_1_pin, w1)
-  GPIO.output(coil_A_2_pin, w2)
-  GPIO.output(coil_B_1_pin, w3)
-  GPIO.output(coil_B_2_pin, w4)
+	time.sleep(delay)
+	GPIO.output(coil_A_1_pin, w1)
+	GPIO.output(coil_A_2_pin, w2)
+	GPIO.output(coil_B_1_pin, w3)
+	GPIO.output(coil_B_2_pin, w4)
 
 # loop through step sequence based on number of steps
 j=0
 for i in range(0, steps):
-        # stop when encoder found
+	# stop when encoder found
 	if GPIO.input(sensor_gpio)==0:
 		break
 	j=j+1
 	if j==1:
-       		setStep(0,0,1,0)
-    	if j==2:
-       		setStep(0,1,1,0)
-    	if j==3:
-      		setStep(0,1,0,0)
-    	if j==4:
-       		setStep(0,1,0,1)
-    	if j==5:
-       		setStep(0,0,0,1)
-    	if j==6:
-       		setStep(1,0,0,1)
-    	if j==7:
-       		setStep(1,0,0,0)
-    	if j==8:
-       		setStep(1,0,1,0)
-       		j=0
+		setStep(0,0,1,0)
+	if j==2:
+		setStep(0,1,1,0)
+	if j==3:
+		setStep(0,1,0,0)
+	if j==4:
+		setStep(0,1,0,1)
+	if j==5:
+		setStep(0,0,0,1)
+	if j==6:
+		setStep(1,0,0,1)
+	if j==7:
+		setStep(1,0,0,0)
+	if j==8:
+		setStep(1,0,1,0)
+		j=0
 
 # move a bit more to compensate an offset between the encoder and the filter center
 setStep(0,0,1,0)
